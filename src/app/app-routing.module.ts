b78c4eab0bc73ce9de9aa520b311508f9538c3c4
Ramiro Olivencia <rolivencia@frsf.utn.edu.ar>
2019-01-22 06:59:20 -0300

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [LoginComponent]
 })
 export class AppRoutingModule { }
