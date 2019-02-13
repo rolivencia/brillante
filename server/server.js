0aec5afb46d4ae0175af0a3f57a69b3ebbb05cee
Ramiro Olivencia <rolivencia@frsf.utn.edu.ar>
2019-02-13 03:07:46 -0300

    Changes for Heroku deploy

--- server/server.js
+++ server/server.js
@@ -10,6 +10,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/brillante'));
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname,'/dist/brillante/index.html'));
+});
+
 // use JWT auth to secure the api
 app.use(jwt());
 
@@ -19,8 +26,11 @@ app.use('/users', require('./users/users.controller'));
 // global error handler
 app.use(errorHandler);
 
-// start server
-const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
-const server = app.listen(port, function () {
-    console.log('Server listening on port ' + port);
-});
+// // start server
+// const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+// const server = app.listen(port, function () {
+//     console.log('Server listening on port ' + port);
+// });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
