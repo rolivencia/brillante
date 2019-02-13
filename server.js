f128371c41a022d21d5d18d83c3810728fee63f5
Ramiro Olivencia <rolivencia@frsf.utn.edu.ar>
2019-02-13 04:53:20 -0300

    Fix in routing to get the client files

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(errorHandler);
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/brillante'));
 
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
   res.sendFile(path.join(__dirname,'/dist/brillante/index.html'));
 });
 
