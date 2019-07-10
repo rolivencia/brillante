560690ea1a479da304ea019c6bae7aef955b007a
Ramiro Olivencia <rolivencia@frsf.utn.edu.ar>
2019-07-10 20:36:48 -0300

    Añadido prettier junto con pre-commit hooks para unificación de estilos de programación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-﻿import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+﻿import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 // used to create fake backend
-import {ErrorInterceptor, JwtInterceptor} from './_helpers';
+import { ErrorInterceptor, JwtInterceptor } from "./_helpers";
 
-import {AppComponent} from './app.component';
-import {routing} from './app.routing';
+import { AppComponent } from "./app.component";
+import { routing } from "./app.routing";
 
-import {AlertComponent} from './_components';
-import {HomeComponent} from './home';
-import {LoginComponent} from './login';
-import {RegisterComponent} from './register';
-import {RepairService} from '@app/_services/repair.service';
+import { AlertComponent } from "./_components";
+import { HomeComponent } from "./home";
+import { LoginComponent } from "./login";
+import { RegisterComponent } from "./register";
+import { RepairService } from "@app/_services/repair.service";
 
 @NgModule({
   imports: [
@@ -32,12 +32,8 @@ import {RepairService} from '@app/_services/repair.service';
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    RepairService,
-
-    // provider used to create fake backend
-    // fakeBackendProvider
+    RepairService
   ],
   bootstrap: [AppComponent]
 })
-
-export class AppModule { }
+export class AppModule {}
