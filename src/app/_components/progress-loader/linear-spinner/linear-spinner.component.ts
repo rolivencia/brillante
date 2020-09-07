bcaac624008e0071fe5a99257a7f89f6c66adb40
Ramiro Olivencia <rolivencia@frsf.utn.edu.ar>
2020-09-07 15:49:12 -0300

    Added progress loader component

---
+++ src/app/_components/progress-loader/linear-spinner/linear-spinner.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'svm-linear-spinner',
+    templateUrl: './linear-spinner.html',
+    styleUrls: ['./linear-spinner.scss'],
+    encapsulation: ViewEncapsulation.None,
+})
+export class LinearSpinnerComponent {
+    constructor() {}
+}
