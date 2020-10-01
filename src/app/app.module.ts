6a07b363dbac30974e89f64f94301882c3cde1d5
Ramiro Olivencia <rolivencia@frsf.utn.edu.ar>
2020-10-01 03:15:30 -0300

    Housekeeping: deleted modules and reassigned providers definitions where required.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,11 @@
 ﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 // used to create fake backend
 import { ErrorInterceptor, JwtInterceptor } from './_helpers';
-
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
-
-import { AlertComponent } from './_components';
 import { DashboardComponent } from './dashboard';
-import { LoginComponent } from './login';
-import { RegisterComponent } from './register';
-import { RepairService } from '@app/_services/repair.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainHeaderComponent } from '@app/_components/main-header/main-header.component';
 import { FooterComponent } from './_components/footer/footer.component';
@@ -33,12 +26,11 @@ import { RegisterModule } from '@app/register/register.module';
         ToastrModule.forRoot(),
         ProgressLoaderModule,
     ],
-    declarations: [AlertComponent, AppComponent, FooterComponent, MainHeaderComponent, DashboardComponent],
+    declarations: [AppComponent, FooterComponent, MainHeaderComponent, DashboardComponent],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
         ProgressLoaderService,
-        RepairService,
     ],
     bootstrap: [AppComponent],
 })
