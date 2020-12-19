251d3334ffa8fcdad9f9a9b15e5771cd7b51edea
Ramiro Olivencia <rolivencia@frsf.utn.edu.ar>
2020-12-19 16:36:47 -0300

    Products: Added calculated categories and manufacturers for left bar filters. Tooling: Updated JavaScript version to ES2020.

--- src/app/landing/products/products-list/products-list-resolver.service.ts
+++ src/app/landing/products/products-list/products-list-resolver.service.ts
@@ -1,16 +1,33 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
-import { Observable } from 'rxjs';
 import { ProductsHttpService } from '@app/landing/products/products.http.service';
-import { Product } from '@app/_models/product';
+import { Category, Manufacturer, Product } from '@app/_models/product';
+import * as _ from 'lodash';
 
 @Injectable({
     providedIn: 'root',
 })
-export class ProductsListResolverService implements Resolve<Product[]> {
+export class ProductsListResolverService
+    implements Resolve<{ list: Product[]; categories: Category[]; manufacturers: Manufacturer[] }> {
     constructor(private productsHttpService: ProductsHttpService) {}
 
-    resolve(route: ActivatedRouteSnapshot): Observable<Product[]> {
-        return this.productsHttpService.getAll();
+    async resolve(
+        route: ActivatedRouteSnapshot
+    ): Promise<{ list: Product[]; categories: Category[]; manufacturers: Manufacturer[] }> {
+        const products = await this.productsHttpService.getAll().toPromise();
+        const categories = _.uniqBy(products.map((product) => product.categories).flat(), 'id');
+        const manufacturers = _.uniqBy(
+            products.map((product) => product.manufacturer),
+            'id'
+        );
+
+        return new Promise((resolve, reject) => {
+            if (products) {
+                resolve({ list: products, categories, manufacturers });
+            } else {
+                console.error(Error);
+                reject(Error);
+            }
+        });
     }
 }
--- src/app/landing/products/products-list/products-list.component.ts
+++ src/app/landing/products/products-list/products-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '@app/_models/product';
+import { ProductsService } from '@app/landing/products/products.service';
 
 @Component({
     selector: 'app-products-list',
@@ -10,11 +11,18 @@ import { Product } from '@app/_models/product';
 export class ProductsListComponent implements OnInit {
     public products: Product[] = [];
 
-    constructor(private route: ActivatedRoute) {}
+    constructor(
+        private changeDetectorRef: ChangeDetectorRef,
+        private productsService: ProductsService,
+        private route: ActivatedRoute
+    ) {}
 
     ngOnInit(): void {
         if (this.route.snapshot.data['products']) {
-            this.products = this.route.snapshot.data['products'];
+            this.products = this.route.snapshot.data['products']['list'];
+            this.productsService.categories = this.route.snapshot.data['products']['categories'];
+            this.productsService.manufacturers = this.route.snapshot.data['products']['manufacturers'];
+            this.changeDetectorRef.detectChanges();
         }
     }
 
