32a440eb45890319317d5986ab56805adada6aaf
Ramiro Olivencia <ramiro@olivencia.com.ar>
2021-09-16 11:47:46 -0300

    Added Sanity connector

--- server/_helpers/environment.js
+++ server/_helpers/environment.js
@@ -7,11 +7,19 @@ if (!('error' in result)) {
     environment = {
         secret: result.parsed.SECRET,
         mySqlDatabase: result.parsed.MYSQL_DATABASE,
+        sanity: {
+            projectId: result.parsed.SANITY_PROJECT_ID,
+            dataset: result.parsed.SANITY_DATASET,
+        },
     };
 } else {
     environment = {
         secret: process.env.SECRET,
         mySqlDatabase: process.env.MYSQL_DATABASE,
+        sanity: {
+            projectId: result.parsed.SANITY_PROJECT_ID,
+            dataset: result.parsed.SANITY_DATASET,
+        },
     };
 }
 
