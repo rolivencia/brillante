e8782364ea56b640ad5c772bb8f5cab1fb1110ed
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-10-17 13:47:52 -0300

    Added social network icons. Added new address to footer. Added brand icons from FontAwesome as dependency. Updated package-lock.json.

--- src/app/_components/footer/footer.component.ts
+++ src/app/_components/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+    instagramIcon: IconDefinition = faInstagram;
+    facebookIcon: IconDefinition = faFacebook;
+    whatsAppIcon: IconDefinition = faWhatsapp;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
