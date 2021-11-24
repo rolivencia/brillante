b3b145ce1b136bcf4b2ee9af93fedfc4cba91027
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-11-24 20:24:05 -0300

    Fixed installments price on product detail

--- src/app/customer-view/products/product-detail/product-detail.component.ts
+++ src/app/customer-view/products/product-detail/product-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '@models/product';
 import { faCartPlus, faCreditCard, faExclamationTriangle, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { PaymentMethodsService } from '@services/payment-methods.service';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-product-detail',
@@ -16,14 +18,20 @@ export class ProductDetailComponent implements OnInit {
 
     public highlightedImageUrl: string = '';
 
-    constructor(private route: ActivatedRoute) {}
+    public $fullPrice: Observable<string>;
+    public $installmentsPrice: Observable<string>;
+
+    constructor(private paymentMethodsService: PaymentMethodsService, private route: ActivatedRoute) {}
 
     ngOnInit(): void {
         if (this.route.snapshot.data['product']) {
             this.product = this.route.snapshot.data['product'];
             this.highlightedImageUrl = this.product.imageUrls[0];
+            this.$fullPrice = this.paymentMethodsService.getPriceWithAppliedFee(this.product.price);
+            this.$installmentsPrice = this.paymentMethodsService.getPriceWithAppliedFee(this.product.price, 12);
         }
     }
+
     setImage(imageUrl: string) {
         this.highlightedImageUrl = imageUrl;
     }
