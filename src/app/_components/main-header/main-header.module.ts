7275c3846dc19c69148625b28b516286849fe970
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-02-18 01:04:56 -0300

    Added separate component to hold the brand container of the header

--- src/app/_components/main-header/main-header.module.ts
+++ src/app/_components/main-header/main-header.module.ts
@@ -6,10 +6,11 @@ import { ListViewModule } from '@syncfusion/ej2-angular-lists';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterModule } from '@angular/router';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrandContainerComponent } from '@components/brand-container/brand-container.component';
 
 @NgModule({
-    declarations: [MainHeaderComponent],
+    declarations: [MainHeaderComponent, BrandContainerComponent],
     imports: [CommonModule, FontAwesomeModule, ListViewModule, RouterModule, SidebarModule, NgbDropdownModule],
-    exports: [MainHeaderComponent],
+    exports: [MainHeaderComponent, BrandContainerComponent],
 })
 export class MainHeaderModule {}
