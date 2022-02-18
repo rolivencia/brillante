74ed6a6ecfc1fa01f3ef55405fb11ad8b26c98be
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-02-18 01:05:17 -0300

    Added separate component to hold the brand container of the header

---
+++ src/app/_components/brand-container/brand-container.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { faBars, faTimes, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+    selector: 'app-brand-container',
+    templateUrl: './brand-container.component.html',
+    styleUrls: ['./brand-container.component.scss'],
+})
+export class BrandContainerComponent implements OnInit {
+    @Input() sidebarOpen: boolean = false;
+    @Output() toggleSidebar: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+    public barsIcon: IconDefinition = faBars;
+    public closeIcon: IconDefinition = faTimes;
+
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    public onToggleSidebar() {
+        this.toggleSidebar.emit(!this.sidebarOpen);
+    }
+}
