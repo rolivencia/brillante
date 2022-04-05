efbd82c4d5602ccb799bee18d503bb1026be3508
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-04-05 18:38:20 -0300

    Replaced Wijmo for SyncFusion Combobox in Repair Add screen

--- src/app/management-view/repair-dashboard/repair-add-new/repair-add-new.module.ts
+++ src/app/management-view/repair-dashboard/repair-add-new/repair-add-new.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RepairAddNewRoutingModule } from './repair-add-new-routing.module';
 import { RepairAddNewComponent } from '@management-view/repair-dashboard/repair-add-new/repair-add-new.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { WjInputModule } from '@grapecity/wijmo.angular2.input';
 import { RepairFormHandlerService } from '@management-view/repair-dashboard/repair-form-handler.service';
-import { CustomerFormModule } from '../../../_components/customer-form/customer-form.module';
+import { CustomerFormModule } from '@components/customer-form/customer-form.module';
+import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
 
 @NgModule({
     declarations: [RepairAddNewComponent],
-    imports: [CommonModule, CustomerFormModule, ReactiveFormsModule, RepairAddNewRoutingModule, WjInputModule],
+    imports: [CommonModule, CustomerFormModule, ReactiveFormsModule, RepairAddNewRoutingModule, DropDownListModule],
     providers: [RepairFormHandlerService],
 })
 export class RepairAddNewModule {}
