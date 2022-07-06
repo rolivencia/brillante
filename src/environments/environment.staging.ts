10b7a212980018b27a7bd2180c4ea1ec8d4791c6
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-07-06 02:34:58 -0300

    Added staging environment for Angular app

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    apiUrl: '',
+    auth0: {
+        domain: 'brillante-staging.us.auth0.com',
+        clientId: 'TvdUyvQnzDAaQ5nypXB5CLW9RpMkFCw1',
+        audience: 'https://brillante-staging.us.auth0.com/api/v2/',
+    },
+};
