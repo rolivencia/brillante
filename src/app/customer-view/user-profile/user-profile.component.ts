468f9834e5b809e0b11b39d316fffdd53f9c9751
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-08-25 13:38:31 -0300

    Added first implementation of user profile template. Added reactive forms for user profile component.

--- src/app/customer-view/user-profile/user-profile.component.ts
+++ src/app/customer-view/user-profile/user-profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '@app/_services';
+import { Observable } from 'rxjs';
+import { User } from '@app/_models';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-register',
@@ -6,7 +10,30 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent implements OnInit {
-    constructor() {}
+    public currentUser$: Observable<User>;
+    public form: FormGroup;
 
-    ngOnInit(): void {}
+    constructor(private authenticationService: AuthenticationService, private formBuilder: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.currentUser$ = this.authenticationService.currentUser.asObservable();
+        this.buildForm();
+    }
+
+    private buildForm() {
+        this.form = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            dni: [null, Validators.required],
+            email: ['', Validators.required],
+            telephone: ['', Validators.required],
+            address: ['', Validators.required],
+        });
+    }
+
+    public onUpdateButtonClicked($event) {
+        const userProfileForm = this.form.value;
+        console.log(userProfileForm);
+        //TODO: Implement this method.
+    }
 }
--- src/app/customer-view/user-profile/user-profile.module.ts
+++ src/app/customer-view/user-profile/user-profile.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { UserProfileRoutingModule } from './user-profile-routing.module';
 import { UserProfileComponent } from './user-profile.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [UserProfileComponent],
-    imports: [CommonModule, UserProfileRoutingModule],
+    imports: [CommonModule, UserProfileRoutingModule, ReactiveFormsModule],
 })
 export class UserProfileModule {}
