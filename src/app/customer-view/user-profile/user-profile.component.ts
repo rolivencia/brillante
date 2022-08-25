8b64c55b8ce5434145fcd73b824852fd1d69e0d8
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-08-25 18:28:48 -0300

    Added populateForm() method

--- src/app/customer-view/user-profile/user-profile.component.ts
+++ src/app/customer-view/user-profile/user-profile.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '@app/_services';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable, of } from 'rxjs';
 import { User } from '@app/_models';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomerService } from '@services/customer.service';
+import { first, switchMap } from 'rxjs/operators';
 
 @Component({
     selector: 'app-register',
@@ -13,11 +15,16 @@ export class UserProfileComponent implements OnInit {
     public currentUser$: Observable<User>;
     public form: FormGroup;
 
-    constructor(private authenticationService: AuthenticationService, private formBuilder: FormBuilder) {}
+    constructor(
+        private authenticationService: AuthenticationService,
+        private customerService: CustomerService,
+        private formBuilder: FormBuilder
+    ) {}
 
     ngOnInit(): void {
         this.currentUser$ = this.authenticationService.currentUser.asObservable();
         this.buildForm();
+        this.populateForm();
     }
 
     private buildForm() {
@@ -29,6 +36,26 @@ export class UserProfileComponent implements OnInit {
             telephone: ['', Validators.required],
             address: ['', Validators.required],
         });
+    }
+
+    private populateForm() {
+        this.currentUser$
+            .pipe(
+                first(),
+                switchMap((user) => {
+                    return combineLatest([of(user), this.customerService.getByEmail(user.email)]);
+                })
+            )
+            .subscribe(([user, customer]) => {
+                this.form.controls['firstName'].setValue(user.firstName);
+                this.form.controls['lastName'].setValue(user.lastName);
+                this.form.controls['email'].setValue(user.email);
+                if (customer) {
+                    this.form.controls['dni'].setValue(customer.dni);
+                    this.form.controls['telephone'].setValue(customer.telephone);
+                    this.form.controls['address'].setValue(customer.address);
+                }
+            });
     }
 
     public onUpdateButtonClicked($event) {
