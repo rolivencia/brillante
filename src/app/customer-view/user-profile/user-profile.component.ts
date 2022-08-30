d88c694764c1e658aae2efbb6834209dfed8ea45
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-08-30 11:23:51 -0300

    Added datepicker to template form. Added update button handler. Injected toaster service for future use.

--- src/app/customer-view/user-profile/user-profile.component.ts
+++ src/app/customer-view/user-profile/user-profile.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from '@app/_services';
+import { AuthenticationService, UserService } from '@app/_services';
 import { combineLatest, Observable, of } from 'rxjs';
 import { User } from '@app/_models';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CustomerService } from '@services/customer.service';
 import { first, switchMap } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+import { MaskPlaceholderModel } from '@syncfusion/ej2-calendars/src/common/maskplaceholder-model';
 
 @Component({
     selector: 'app-register',
@@ -12,13 +14,18 @@ import { first, switchMap } from 'rxjs/operators';
     styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent implements OnInit {
+    public maskPlaceholderValue: MaskPlaceholderModel = { day: 'DD', month: 'MM', year: 'AAAA' };
+
     public currentUser$: Observable<User>;
     public form: FormGroup;
+    public submitted: boolean = false;
 
     constructor(
         private authenticationService: AuthenticationService,
         private customerService: CustomerService,
-        private formBuilder: FormBuilder
+        private formBuilder: FormBuilder,
+        private toastService: ToastrService,
+        private userService: UserService
     ) {}
 
     ngOnInit(): void {
@@ -29,12 +36,14 @@ export class UserProfileComponent implements OnInit {
 
     private buildForm() {
         this.form = this.formBuilder.group({
+            avatar: ['', Validators.required],
             firstName: ['', Validators.required],
             lastName: ['', Validators.required],
             dni: [null, Validators.required],
             email: ['', Validators.required],
             telephone: ['', Validators.required],
             address: ['', Validators.required],
+            birthDate: ['', Validators.required],
         });
     }
 
@@ -47,6 +56,7 @@ export class UserProfileComponent implements OnInit {
                 })
             )
             .subscribe(([user, customer]) => {
+                this.form.controls['avatar'].setValue(user.avatar ? user.avatar : '');
                 this.form.controls['firstName'].setValue(user.firstName);
                 this.form.controls['lastName'].setValue(user.lastName);
                 this.form.controls['email'].setValue(user.email);
@@ -54,13 +64,36 @@ export class UserProfileComponent implements OnInit {
                     this.form.controls['dni'].setValue(customer.dni);
                     this.form.controls['telephone'].setValue(customer.telephone);
                     this.form.controls['address'].setValue(customer.address);
+                    this.form.controls['birthDate'].setValue(customer.birthDate);
                 }
             });
     }
 
-    public onUpdateButtonClicked($event) {
-        const userProfileForm = this.form.value;
-        console.log(userProfileForm);
-        //TODO: Implement this method.
+    public onUpdateButtonClicked(form: FormGroup) {
+        this.submitted = true;
+        if (form.invalid) {
+            return;
+        }
+
+        const userProfileForm = form.value;
+
+        this.userService.updateCustomerUser(
+            {
+                firstName: userProfileForm.firstName,
+                lastName: userProfileForm.lastName,
+                avatar: userProfileForm.avatar,
+                userName: `${userProfileForm.firstName}_${userProfileForm.lastName}`, //FIXME: Build a username based on firstName and lastName
+                email: userProfileForm.email,
+            },
+            {
+                firstName: userProfileForm.firstName,
+                lastName: userProfileForm.lastName,
+                dni: userProfileForm.dni,
+                telephone: userProfileForm.telephone,
+                address: userProfileForm.address,
+                birthDate: userProfileForm.birthDate,
+                email: userProfileForm.email,
+            }
+        );
     }
 }
--- src/app/customer-view/user-profile/user-profile.module.ts
+++ src/app/customer-view/user-profile/user-profile.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { UserProfileRoutingModule } from './user-profile-routing.module';
 import { UserProfileComponent } from './user-profile.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DatePickerModule } from '@syncfusion/ej2-angular-calendars';
 
 @NgModule({
     declarations: [UserProfileComponent],
-    imports: [CommonModule, UserProfileRoutingModule, ReactiveFormsModule],
+    imports: [CommonModule, UserProfileRoutingModule, ReactiveFormsModule, DatePickerModule],
 })
 export class UserProfileModule {}
