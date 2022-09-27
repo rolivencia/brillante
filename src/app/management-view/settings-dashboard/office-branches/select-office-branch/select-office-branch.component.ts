c30a0be973e5ff052be1e1e5f7358820ed5f6726
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-09-26 23:53:27 -0300

    [#339] - Moved logic to select an office branch to the select-office-branch component

--- src/app/management-view/settings-dashboard/office-branches/select-office-branch/select-office-branch.component.ts
+++ src/app/management-view/settings-dashboard/office-branches/select-office-branch/select-office-branch.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FieldSettingsModel } from '@syncfusion/ej2-angular-dropdowns';
+import { OfficeBranch } from '@models/office-branch';
+import { OfficeBranchService } from '@services/office-branch.service';
+import { ToastrService } from 'ngx-toastr';
+import { first } from 'rxjs/operators';
 
 @Component({
     selector: 'app-select-office-branch',
@@ -6,7 +12,43 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./select-office-branch.component.scss'],
 })
 export class SelectOfficeBranchComponent implements OnInit {
-    constructor() {}
+    public form: FormGroup;
+    public officeBranchFields: FieldSettingsModel = { text: 'name', value: 'id' };
+    public officeBranches: OfficeBranch[] = [];
+    public selectedBranch: OfficeBranch = new OfficeBranch();
 
-    ngOnInit(): void {}
+    constructor(
+        public officeBranchService: OfficeBranchService,
+        private formBuilder: FormBuilder,
+        private toastrService: ToastrService
+    ) {}
+
+    ngOnInit(): void {
+        this.buildForm();
+        this.load();
+    }
+
+    public assign() {
+        const selectedIdBranch = this.form.get('idBranch').value;
+        const selectedBranch = this.officeBranches.filter((o) => o.id === selectedIdBranch).pop();
+        this.officeBranchService.assign(selectedBranch);
+        this.toastrService.success(`Sucursal ${selectedBranch.name} asignada correctamente.`);
+    }
+
+    private buildForm() {
+        this.form = this.formBuilder.group({ idBranch: [0, Validators.required] });
+    }
+
+    private load() {
+        this.officeBranchService
+            .fetch()
+            .pipe(first())
+            .subscribe((branches) => {
+                this.officeBranches = branches;
+                this.selectedBranch = this.officeBranchService.current.value
+                    ? this.officeBranchService.current.value
+                    : branches[0];
+                this.form.get('idBranch').setValue(this.selectedBranch.id);
+            });
+    }
 }
