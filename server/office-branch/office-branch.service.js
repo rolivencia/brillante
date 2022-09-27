501c7b93255621c8826eea9a980adb958014429c
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-09-27 01:47:50 -0300

    [#339] - Added endpoint and method for creation of new OfficeBranch records

--- server/office-branch/office-branch.service.js
+++ server/office-branch/office-branch.service.js
@@ -1,9 +1,18 @@
-const officeBranch = require('server/office-branch/office-branch.model');
+const { OfficeBranch } = require('server/office-branch/office-branch.model');
 
 module.exports = {
     get,
+    create,
 };
 
 function get() {
-    return officeBranch.OfficeBranch.findAll();
+    return OfficeBranch.findAll();
+}
+
+function create(branch) {
+    return OfficeBranch.create({
+        name: branch.name,
+        codeName: branch.name.toLowerCase().split(' ').join('_'),
+        address: branch.address,
+    });
 }
