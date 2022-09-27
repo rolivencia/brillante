66ac16ede52cfa8e88677ced3ae73f6ce44f4b7b
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-09-27 01:08:54 -0300

    [#339] - Added body of onSave method

--- src/app/management-view/settings-dashboard/office-branches/add-office-branch/add-office-branch.component.ts
+++ src/app/management-view/settings-dashboard/office-branches/add-office-branch/add-office-branch.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 import { OfficeBranch } from '@models/office-branch';
 import { Observable, of } from 'rxjs';
 import { OfficeBranchService } from '@services/office-branch.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'app-add-office-branch',
@@ -13,7 +14,11 @@ export class AddOfficeBranchComponent implements OnInit {
     public form: FormGroup;
     public officeBranches$: Observable<OfficeBranch[]> = of();
 
-    constructor(private formBuilder: FormBuilder, private officeBranchService: OfficeBranchService) {}
+    constructor(
+        private formBuilder: FormBuilder,
+        private officeBranchService: OfficeBranchService,
+        private toastrService: ToastrService
+    ) {}
 
     ngOnInit(): void {
         this.officeBranches$ = this.officeBranchService.fetch();
@@ -24,6 +29,13 @@ export class AddOfficeBranchComponent implements OnInit {
     }
 
     onSave(event) {
-        console.log(this.form.value);
+        if (this.form.invalid) {
+            this.toastrService.error(`Los datos ingresados son inválidos.`);
+        }
+
+        this.officeBranchService.create(this.form.value).subscribe((branch) => {
+            this.toastrService.success(`Creada nueva sucursal "${branch.name}"`);
+            this.officeBranches$ = this.officeBranchService.fetch();
+        });
     }
 }
