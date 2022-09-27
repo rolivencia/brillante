7f4bced244bc3b043c5145054006fa6c6da90406
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-09-26 23:55:38 -0300

    [#339] - Added router-outlet to load the child components of offices-branches component

--- src/app/management-view/settings-dashboard/office-branches/office-branches-routing.module.ts
+++ src/app/management-view/settings-dashboard/office-branches/office-branches-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OfficeBranchesComponent } from './office-branches.component';
+import { SelectOfficeBranchComponent } from '@management-view/settings-dashboard/office-branches/select-office-branch/select-office-branch.component';
 
-const routes: Routes = [{ path: '', component: OfficeBranchesComponent }];
+const routes: Routes = [
+    { path: '', component: OfficeBranchesComponent, children: [{ path: '', component: SelectOfficeBranchComponent }] },
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
