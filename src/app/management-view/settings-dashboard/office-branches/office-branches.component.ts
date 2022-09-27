904180f161150456dc355c2c1ed55926766c3c0b
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-09-27 00:54:20 -0300

    [#339] - Reorganized component structure to make office branches grid visible for all children

--- src/app/management-view/settings-dashboard/office-branches/office-branches.component.ts
+++ src/app/management-view/settings-dashboard/office-branches/office-branches.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { OfficeBranch } from '@models/office-branch';
+import { OfficeBranchService } from '@services/office-branch.service';
 
 @Component({
     selector: 'app-office-branches',
@@ -7,9 +10,17 @@ import { ActivatedRoute, Router } from '@angular/router';
     styleUrls: ['./office-branches.component.scss'],
 })
 export class OfficeBranchesComponent implements OnInit {
-    constructor(private route: ActivatedRoute, private router: Router) {}
+    public officeBranches$: Observable<OfficeBranch[]> = of();
 
-    ngOnInit(): void {}
+    constructor(
+        private officeBranchService: OfficeBranchService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) {}
+
+    ngOnInit(): void {
+        this.officeBranches$ = this.officeBranchService.fetch();
+    }
 
     goToAdd($event) {
         this.router.navigate(['add'], { relativeTo: this.route });
