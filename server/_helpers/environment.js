80b256d055a5ba3d25c17faa123867031a05cf09
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-08-12 16:55:39 -0300

    [#363] - Remove Sanity.io usages and helpers from backend project

--- server/_helpers/environment.js
+++ server/_helpers/environment.js
@@ -1,10 +1,6 @@
 const environment = {
     secret: process.env.SECRET,
     mySqlDatabase: process.env.MYSQL_DATABASE,
-    sanity: {
-        projectId: process.env.SANITY_PROJECT_ID,
-        dataset: process.env.SANITY_DATASET,
-    },
 };
 
 module.exports = environment;
