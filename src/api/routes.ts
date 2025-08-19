62ec2a98e343305188d890b259b4de5d03a0da2d
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-08-19 19:29:08 -0300

    [#380] - Migrate server routes declaration

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,4 +1,12 @@
-import express from 'express';
+import { Router } from 'express';
 
-export type TControlledApiRoute = { path: 'string', controller: express.Router }
-export default [] as TControlledApiRoute[];
+export type TControlledApiRoute = { path: 'string', controller: Router }
+export default [
+  // { path: '/users', controller: '' },
+  // { path: '/office-branch', controller: '' },
+  // { path: '/client', controller: '' },
+  // { path: '/repair', controller: '' },
+  // { path: '/cash', controller: '' },
+  // { path: '/cash/transaction', controller: '' },
+  // { path: '/products', controller: '' },
+] as TControlledApiRoute[];
