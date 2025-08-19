e74e5ee5dc84aefe56d4332dc4d87b5a68390c27
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-08-19 19:43:13 -0300

    [#380] - Add environment field to environment object

--- src/api/_helpers/environment.ts
+++ src/api/_helpers/environment.ts
@@ -1,11 +1,13 @@
+type EnvironmentType = 'development' | 'staging' | 'production'
+
 interface Environment {
     secret: string;
     mySqlDatabase: string;
+    environment: EnvironmentType
 }
 
-const environment: Environment = {
-    secret: process.env["SECRET"] || '',
-    mySqlDatabase: process.env["MYSQL_DATABASE"] || '',
-};
-
-export default environment;
+export default {
+  secret: process.env["SECRET"] || '',
+  mySqlDatabase: process.env["MYSQL_DATABASE"] || '',
+  environment: process.env['ENVIRONMENT'] as EnvironmentType || 'development'
+} satisfies Environment;
